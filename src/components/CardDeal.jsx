7d06908cd2fb875e39576954b6e1e68a9f382fbc
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-05-16 01:41:34 +0530

    done with our app

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles, {layout} from '../style'
+import Button from './Button'
+import { card } from '../assets'
+
+
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/>In few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Arcy roreir, purwrs IN sfdsiji, set ser m integer, sifhs Adsfiosf, quis alowuest eege werir, oihtwo.Asidf isfser ,etc</p>
+      <Button styles="mt-10"/>   
+      </div>
+      {/*  ----------iamge section ------------------ */}
+      <div className={layout.sectionImg}>
+        <img src={card} className='w-[100%] h-[100%]'/>
+      </div>
+    </section>
   )
 }
 
